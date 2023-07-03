a84f0bb0e3146759fc540bfb0ce5cb65637d08c5
GastonPeralta93 <gastonperalta93@gmail.com>
2023-07-03 20:18:21 -0300

    Creación inicial de la app

---
+++ src/main/java/com/mercado/circular/MercadoCircularApplication.java
@@ -0,0 +1,13 @@
+package com.mercado.circular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoCircularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoCircularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercado/circular/MercadoCircularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.circular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoCircularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
